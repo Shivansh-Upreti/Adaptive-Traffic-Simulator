93064e13b6144669ad54962732dc99c52462ebe2
pallavilingwal12 <pallavihi12@gmail.com>
2026-09-04 12:32:42 +0530

    Stack header file added

---
+++ ArrayStack.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+class ArrayStack{
+    int data[32];
+    int top;
+    public:
+        ArrayStack(){
+            top = -1;
+        }
+
+        bool isEmpty(){
+            return top == -1;
+        }
+
+        bool isFull(){
+            return top == 31;
+        }
+
+        int size(){
+            return top + 1;
+        }
+
+        void push(int value){
+            if (isFull()){
+                return;
+            }
+            top++;
+            data[top] = value;
+        }
+
+        int pop(){
+            if (isEmpty()){
+                return -1;
+            }
+            int value = data[top];
+            top--;
+            return value;
+        }
+
+        int top(){
+            if (isEmpty()){
+                return -1;
+            }
+            return data[top];
+        }
+};
